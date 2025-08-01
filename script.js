90f51edf1648cd3273857721dc2f20b78b6e1fa6
emilin <164918748+emilinvitoria@users.noreply.github.com>
2025-08-01 15:04:03 -0300

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+unction inserirNome(){
+let nomeUsuario = prompt("Qual o seu nome");
+   let elemento = document.querySelector("#nome-usuario");
+   console.log(elemento);
+   elemento.textContent = nomeUsuario;
+}
+
+const lista = document.querySelector('#lista');
+
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagens[2];
